d92359f6129c6d1e2b0aeedae517d68a5e6ae26c
Abhishek-Singh2003 <abhisheksingh13722@gmail.com>
2025-11-13 15:56:59 +0530

    this ius new branch created

--- src/just_testing/classA.java
+++ src/just_testing/classA.java
@@ -9,6 +9,7 @@ public static void main(String[] args)
 	System.out.println("hello world");
 	System.out.println("ghjk");
 	System.err.println("hello");
+	System.out.println("thios is explain in natrajna sir ");
 }
 
 }
